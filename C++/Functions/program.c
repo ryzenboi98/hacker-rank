ebdbd5b58d9e4e6f9e85c793df9a452759c37ca0
João António <49904539+ryzenboi98@users.noreply.github.com>
2020-12-03 20:23:22 +0000

    Create program.c

---
+++ C++/Functions/program.c
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d) {
+    int numbs[4];
+    
+    numbs[0] = a;
+    numbs[1] = b;
+    numbs[2] = c;
+    numbs[3] = d;
+
+    int max = numbs[0];
+    
+    for(int i = 1; i < sizeof(*numbs); i++)
+        if(max < numbs[i]) 
+            max = numbs[i];
+
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
